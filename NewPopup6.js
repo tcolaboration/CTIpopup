48164aa89ef690ae76c6f2388194a920fb628f1b
tcolaboration <43301415+tcolaboration@users.noreply.github.com>
2022-02-23 10:12:15 -0800

    Add files via upload

---
+++ NewPopup6.js
@@ -0,0 +1,14 @@
+function openRequestedPopup(){
+console.log("===========TEST=========");
+var windowObjectReference;
+var windowFeatures = "popup";
+var callNumber =  document.getElementById("secondaryInfo").innerHTML
+var url = "https://eqiq-dev1-odcsvbcs-09301414-3450-demo1820784.builder.ocp.oraclecloud.com/ic/builder/rt/ReportIssue/1.0/webApps/logsr/?PhoneNumber="+callNumber;
+windowObjectReference = window.open(, "CNN_WindowName", windowFeatures);  
+console.log(callNumber);
+}
+
+
+
+
+

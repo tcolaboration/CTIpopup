c9766a9e61f0ae285919b4658d0a2e9935ffdd2c
tcolaboration <43301415+tcolaboration@users.noreply.github.com>
2022-02-23 09:28:41 -0800

    Add files via upload

---
+++ NewPopup.js
@@ -0,0 +1,4 @@
+function popup(){
+console.log("===========TEST=========");
+  window.open("https://eqiq-dev1-odcsvbcs-09301414-3450-demo1820784.builder.ocp.oraclecloud.com/ic/builder/rt/ReportIssue/1.0/webApps/logsr/", '_blank').focus();
+}
